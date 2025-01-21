73e78cdfa9711c5c1777ba16ec4ced38734f50a6
iamalexninov <iamalexgninov@gmail.com>
2025-01-21 23:49:50 +0200

    plan relational database integration - MySQL

--- config/database.js
+++ config/database.js
@@ -1,11 +1,1 @@
-const mongoose = require("mongoose");
-
-module.exports = async () => {
-  try {
-    mongoose.connect(process.env.DB_URL, {});
-    console.log("Run Database...");
-  } catch (error) {
-    console.error(error);
-    console.log("Can't Run Database...");
-  }
-};
+// Rewrite the project using MySQL as database against with Prisma.
